2b30250e0cfaabb78b6c0025ff4e8fb906a278a6
LCJ-MinYa <1049468118@qq.com>
2017-05-18 19:33:37 +0800

    封装请求超时

--- app/utils/request.js
+++ app/utils/request.js
@@ -118,7 +118,7 @@ let request = React.createClass({
      *  callback:回调函数
      *  isLoading:是否显示加载状态
      */
-	PostService(url, params, callback){
+	PostService(url, params, callback, timeout){
 		let URL = config.API + url;
 		params.timestamp = this.getTimestamp();
 		Storage.getData('token')
@@ -138,7 +138,44 @@ let request = React.createClass({
 			}
 			params.signstr = this.getSingnStr(params);
 			//发送POST请求
-			return fetch(URL, {
+			this._fetch(this.fetch_promise(url, URL, params, callback), 2000)
+			.then((data) =>{
+				callback(data);
+			})
+			.catch((err) => {
+				console.log('请求超时失败');
+			})
+		})
+	},
+
+	/*
+     * 封装_fetch方法，使其可以设置timeout
+     * @param timeout 过期时间
+     * @return
+     */
+     _fetch(fetch_promise, timeout){
+     	console.log(11);
+		var abort_fn = null;
+    	var abort_promise = new Promise((resolve, reject) => {
+        	abort_fn = function() {
+        		console.log('请求准备超时');
+            	reject('abort promise');
+        	};
+    	});
+		var abortable_promise = Promise.race([fetch_promise, abort_promise]);
+    	setTimeout(function(){abort_fn();}, timeout);
+    	return abortable_promise;
+    },
+
+	/*
+     * 封装_fetch方法，使其可以设置timeout
+     * @param timeout 过期时间
+     * @return
+     */
+    fetch_promise(url, URL, params, callback) {
+    	console.log("开始请求");
+    	return new Promise((resolve, reject) => {
+			fetch(URL, {
 				method: 'POST',
 				headers: {
 					'Accept': 'application/json',
@@ -146,27 +183,34 @@ let request = React.createClass({
 				},
 				body: this.toQueryString(params)
 			})
-		})
-		.then((result) => result.json())
-		.then((resultJSON) => {
-			if(url == '/api/system/getGlobalInfo' && resultJSON.error_code < 0){
-				callback(resultJSON);
-			}else if(resultJSON.error_code < 0){
-                Alert.alert('请求失败',resultJSON.error_message,[
-                    {
-                        text: '确定',
-                    }
-                ])
-			}else{
-				if(callback != undefined){
-					callback(resultJSON);
+			.then((result) => {
+				return result.json();
+			})
+			.then((resultJSON) => {
+				if(url == '/api/system/getGlobalInfo' && resultJSON.error_code < 0){
+					resolve(resultJSON);
+				}else if(resultJSON.error_code < 0){
+	                Alert.alert('请求失败',resultJSON.error_message,[
+	                    {
+	                        text: '确定',
+	                    }
+	                ])
+				}else{
+					if(callback != undefined){
+						resolve(resultJSON);
+					}
 				}
-			}
-		})
-		//捕获fetch错误异常
-		.catch((error) => {
-        	console.error(error);
-      	});
+			})
+			//捕获fetch错误异常
+	        .catch((err) => {
+	        	reject(err);
+	            if (err.message === 'Network request failed'){
+	                console.log('网络出错');
+	            } else if (err === 'abort promise'){
+	                console.log('请求超时');
+	            }
+	        })
+	    })
 	}
 });
 
